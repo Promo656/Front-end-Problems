1ef0e35dedafd2169c2fce9a34c2d34190fa06f8
egor <rank2007@mail.ru>
2022-12-16 13:50:01 +0300

    add solution 4

--- Binary search.js
+++ Binary search.js
@@ -71,6 +71,38 @@ const solution3 = (arr) => {
     return arr[end]
 }
 
-console.time("solution3");
-console.log(solution3([4, 6, 8, 9, 10, 12, 13, 14, 15, 27, 18, 23, 24, 26, 27, 29, 34, 35, 37, 38, 2,]));
-console.timeEnd("solution3");
+// console.time("solution3");
+// console.log(solution3([4, 6, 8, 9, 10, 12, 13, 14, 15, 27, 18, 23, 24, 26, 27, 29, 34, 35, 37, 38, 2,]));
+// console.timeEnd("solution3");
+
+
+const Solution4 = (a1, a2, n, m) => {
+    const sortedArr = a1.sort((a, b) => a - b);
+    let result = [];
+    let start = 0;
+    let end = sortedArr.length - 1;
+    let mid;
+    for (let i = 0; i < m; i++) {
+        while (end - start >= 0) {
+            mid = start + Math.floor((end - start) / 2);
+
+            if (sortedArr[mid] === a2[i]) {
+                result.push(a2[i]);
+                sortedArr.splice(mid, 1)
+                start = 0;
+                end = sortedArr.length - 1
+                break;
+            } else if (sortedArr[mid] > a2[i]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+    }
+    return result.length === m
+        ? "Yes"
+        : "No";
+}
+console.time("solution4");
+console.log(Solution4([11, 1, 13, 21, 3, 7], [11, 3, 7, 1], 6, 4));
+console.timeEnd("solution4");
