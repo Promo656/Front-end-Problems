f7805bade17f529d0049c97872327e28c298406c
egor <rank2007@mail.ru>
2022-12-27 07:14:29 +0300

    add insertionSort solution 1

--- SortAlgorithms/insertionSort.js
+++ SortAlgorithms/insertionSort.js
@@ -1,16 +1,36 @@
 const insertionSort = (arr) => {
     let len = arr.length;
-    debugger
+    let currentValue
     for (let i = 1; i < len; i++) {
-        let currentValue = arr[i];
         let j;
+        currentValue = arr[i];
 
-        for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+        for (j = i - 1; j >= 0 && currentValue < arr[j]; j--) {
             arr[j + 1] = arr[j];
         }
         arr[j + 1] = currentValue;
     }
     return arr;
 }
+// console.log(insertionSort([2,1,4,3,5,4,8,6]))
 
-console.log(insertionSort([2, 1, 4, 3, 6, 5]))
+//--------------------------------------------------------------------------------------------//
+
+const solution1 = (arr) => {
+    let len = arr.length;
+    let currentValue;
+    let count = 0;
+    for (let i = 1; i < len; i++) {
+        let j;
+        currentValue = arr[i];
+
+        for (j = i - 1; j >= 0 && currentValue < arr[j]; j--) {
+            arr[j + 1] = arr[j]
+            count += 1;
+        }
+        arr[j + 1] = currentValue;
+    }
+    return count;
+}
+
+// console.log(solution1([2, 1, 4, 3, 6, 5]))
