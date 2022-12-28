2bfca6d7f82e4c255941bdefcecbb190c7a8d1bd
egor <rank2007@mail.ru>
2022-12-28 13:25:54 +0300

    add insertionSort solution 2

--- SortAlgorithms/insertionSort.js
+++ SortAlgorithms/insertionSort.js
@@ -32,5 +32,32 @@ const solution1 = (arr) => {
     }
     return count;
 }
-
 // console.log(solution1([2, 1, 4, 3, 6, 5]))
+
+//--------------------------------------------------------------------------------------------//
+
+const solution2 = (nums) => {
+    const squareArray = [];
+
+    nums.forEach(el => squareArray.push(el ** 2));
+
+    const sort = (arr) => {
+        let len = arr.length;
+        let currentValue, j;
+
+        for (let i = 1; i < len; i++) {
+            currentValue = arr[i];
+
+            for (j = i - 1; j >= 0 && currentValue < arr[j]; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = currentValue;
+        }
+        return arr;
+    }
+
+    return sort(squareArray);
+};
+// console.log(solution2([-4,-1,0,3,10]))
+
+//--------------------------------------------------------------------------------------------//
