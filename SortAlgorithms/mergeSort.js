4deebd4cbcbc09eaaf2578029792232dd7697de4
egor <rank2007@mail.ru>
2022-12-30 08:39:24 +0300

    add mergeSort solution 1

--- SortAlgorithms/mergeSort.js
+++ SortAlgorithms/mergeSort.js
@@ -19,3 +19,24 @@ const mergeSort = (arr) => {
 
     return merge(left, right);
 }
+// console.log(mergeSort([2,1,4,3]))
+
+//--------------------------------------------------------------------------------------------//
+
+const solution1 = (nums1, m, nums2, n) => {
+    const merge = (left, right) => {
+        let sortedArr = [];
+        while (left.length && right.length) {
+            if (left[0] < right[0]) {
+                sortedArr.push(left.shift())
+            } else {
+                sortedArr.push(right.shift())
+            }
+        }
+        return [...sortedArr, ...left, ...right]
+    }
+
+    return merge(nums1.slice(0, m), nums2.slice(0, n))
+}
+const nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
+console.log(merge(nums1.slice(0, m), nums2.slice(0, n)));
