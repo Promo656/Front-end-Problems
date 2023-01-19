2b427f810e81753ab210608fa27a2aaec5a15475
egor <rank2007@mail.ru>
2023-01-19 06:02:09 +0300

    implement Function.prototype.bind

--- jsUtils/jsUtils.js
+++ jsUtils/jsUtils.js
@@ -140,3 +140,20 @@ Array.prototype.myFilter = function (callbackFn, thisArg) {
 
     return result;
 };
+
+// Implement Function.prototype.bind
+Function.prototype.myBind = function (thisArg, ...boundArgs) {
+    debugger
+    const originalFunc = this;
+    if (typeof originalFunc !== 'function') {
+        throw new TypeError('Bind must be called on a function');
+    }
+    return function (...args) {
+        debugger
+        return Function.prototype.apply.call(originalFunc, thisArg, [
+            ...boundArgs,
+            ...args,
+        ]);
+    };
+};
+
