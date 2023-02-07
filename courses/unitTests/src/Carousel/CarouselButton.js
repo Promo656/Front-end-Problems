3f5710cc44668facb219f31376884203e4b31de6
egor <rank2007@mail.ru>
2023-02-07 07:21:49 +0300

    add Testing Page Components

--- courses/unitTests/src/Carousel/CarouselButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// START_HIGHLIGHT
-const CarouselButton = props => <button {...props} />; //<callout id="co.spread-props" />
-// END_HIGHLIGHT
-
-export default CarouselButton;
