7e262896c3dab8b92b133b1d2d5ee00375b121da
egor <rank2007@mail.ru>
2023-02-14 11:31:14 +0300

    fix naming mistake

--- courses/unitTests/__integration-test__/create-task.test.ts
+++ courses/unitTests/__integration-test__/create-task.test.ts
@@ -8,6 +8,6 @@ afterAll(async () => await driver.quit());
 test("should create tasks", async () => {
     await driver.get('http://localhost:3000');
     await driver.wait(until.elementsLocated(By.xpath("//input[@placeholder='Enter new task']")), 1000)
-    await driver.findElement(By.xpath("//input[@placeholder='Enter new task']")).sendKeys("new todo" + Key.ENTER)
+    await driver.findElement(By.xpath("//input[@placeholder='Enter new task']")).sendKeys("new todo!" + Key.ENTER)
     await driver.wait(until.elementsLocated(By.xpath("//*[text()='new todo!']")), 1000)
 })
--- courses/unitTests/src/App.tsx
+++ courses/unitTests/src/App.tsx
@@ -1,11 +1,14 @@
 import {Input} from "./components/Input";
 import {TaskList} from "./components/TaskList";
+import {useState} from "react";
 
 export const App = () => {
+    const [tasks, setTasks] = useState<Array<string>>([])
+    const handleSubmit = (task) => setTasks([...tasks, task])
     return (
         <div>
-            <Input onSubmit={()=>{}}/>
-            <TaskList tasks={["A","B","C"]}/>
+            <Input onSubmit={handleSubmit}/>
+            <TaskList tasks={tasks}/>
         </div>
     )
 }
