1f36855fe7ce3b4c9be7f779066920d7a9af4af7
egor <rank2007@mail.ru>
2023-03-06 08:31:21 +0300

    add test for binary search with mirror array

--- dsa/searchAlgorithms/binarySearch/binarySearch.test.js
+++ dsa/searchAlgorithms/binarySearch/binarySearch.test.js
@@ -1,17 +1,34 @@
-const binarySearch = require("./index");
+const {commonBinarySearch, mirrorBinarySearch} = require("./index");
 
-test("Binary search algorithm should find the target index", () => {
-    const sortedArray = [-10, -4, 0, 3, 6, 8, 9, 23, 45, 78]
-    const sortedArrayWithSingleElement = [4]
-    const emptyArray = []
+describe("Binary search algorithm", () => {
+    it("should find the target index in common sorted array", () => {
+        const array = [-10, -4, 0, 3, 6, 8, 9, 23, 45, 78]
+        const arrayWithSingleElement = [4]
+        const emptyArray = []
 
-    const found = binarySearch(sortedArray, 23)
-    const notFoundInFilledArray = binarySearch(sortedArray, 100)
-    const foundSingleElement = binarySearch(sortedArrayWithSingleElement, 4)
-    const notFoundInEmptyArray = binarySearch(emptyArray, 3)
+        const found = commonBinarySearch(array, 23)
+        const notFoundInFilledArray = commonBinarySearch(array, 100)
+        const foundSingleElement = commonBinarySearch(arrayWithSingleElement, 4)
+        const notFoundInEmptyArray = commonBinarySearch(emptyArray, 3)
 
-    expect(found).toBe(7)
-    expect(notFoundInFilledArray).toBe(-1)
-    expect(foundSingleElement).toBe(0)
-    expect(notFoundInEmptyArray).toBe(-1)
+        expect(found).toBe(7)
+        expect(notFoundInFilledArray).toBe(-1)
+        expect(foundSingleElement).toBe(0)
+        expect(notFoundInEmptyArray).toBe(-1)
+    });
+
+    it("should find the max number in common array in mirror array", () => {
+        const array = [1, 2, 5, 7, 9, 13, 16, 27, 46, 78, 90, 43];
+        const arrayWithSingleElement = [4]
+        const emptyArray = []
+
+        const found = mirrorBinarySearch(array)
+        const foundSingleElement = mirrorBinarySearch(arrayWithSingleElement)
+        const notFoundInEmptyArray = mirrorBinarySearch(emptyArray)
+
+        expect(found).toBe(90)
+        expect(foundSingleElement).toBe(4)
+        expect(notFoundInEmptyArray).toMatch(/not found/i)
+
+    })
 })
--- dsa/searchAlgorithms/binarySearch/index.js
+++ dsa/searchAlgorithms/binarySearch/index.js
@@ -1,4 +1,4 @@
-module.exports = (array, target) => {
+const commonBinarySearch = (array, target) => {
     const length = array.length
     let start = 0;
     let end = length - 1
@@ -16,4 +16,29 @@ module.exports = (array, target) => {
         }
     }
     return -1;
+}
+
+const mirrorBinarySearch = (array) => {
+    const length = array.length;
+    if (length === 0) {
+        return "Not found"
+    }
+    let start = 0;
+    let end = length - 1
+    let middle
+
+    while (start < end) {
+        middle = start + Math.floor((end - start) / 2)
+        if (array[middle] > array[middle + 1]) {
+            end = middle
+        } else {
+            start = middle + 1
+        }
+    }
+    return array[end]
+}
+
+module.exports = {
+    commonBinarySearch,
+    mirrorBinarySearch
 }
